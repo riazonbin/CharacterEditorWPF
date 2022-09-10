c352b4a96c940f2e0bf5657624ae94bc86bd60a9
riazonbin <riazonbin@mail.ru>
2022-09-10 08:54:36 +0300

    Добавьте файлы проекта.

---
+++ CharacterEditorCore/Unit.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CharacterEditorCore
+{
+    public abstract class Unit
+    {
+        public double strength;
+        public double dexterity;
+        public double constitution;
+        public double intelligence;
+        public double manaPoints;
+        public double healthPoints;
+    }
+}
