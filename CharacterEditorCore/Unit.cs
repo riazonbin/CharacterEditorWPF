63d7ea42bc2c008d05403d11cd676a735de948ad
riazonbin <riazonbin@mail.ru>
2022-09-10 09:15:33 +0300

    added Enums with characters stat's

--- CharacterEditorCore/Unit.cs
+++ CharacterEditorCore/Unit.cs
@@ -8,11 +8,19 @@ namespace CharacterEditorCore
 {
     public abstract class Unit
     {
-        public double strength;
-        public double dexterity;
-        public double constitution;
-        public double intelligence;
+        public int strength
+        {
+            get; set;
+        }
+        public int dexterity { get; set; }
+        public int constitution { get; set; }
+        public int intelligence;
+        public int availablePoints;
+
         public double manaPoints;
         public double healthPoints;
+        public double attack;
+        public double physicalDefense;
+        public double magicAttack;
     }
 }
