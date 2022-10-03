e5daff4f6c66443f9b30a3ccab0e9fda9d5a07ab
riazonbin <riazonbin@mail.ru>
2022-10-03 13:14:48 +0300

    added equipment class

---
+++ CharacterEditorCore/Equipment/Equipment.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CharacterEditorCore.Equipment
+{
+    public abstract class Equipment
+    {
+        public int RequiredStrength { get; set; }
+
+        public int RequiredDexterity { get; set; }
+
+        public int RequiredConstitution { get; set; }
+
+        public int RequiredIntelligence { get; set; }
+
+        public int HealthPointsBuff { get; set; }
+
+        public int ManaPointsBuff { get; set; }
+
+        public int AttackBuff { get; set; }
+
+        public int MagicAttackBuff { get; set; }
+
+        public int PhysicalDefBuff { get; set; }
+
+        public int EquipmentLevel { get; set; }
+    }
+}
