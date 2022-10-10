a5488617a01239d011b242c8b5ffc9e258ae59fe
riazonbin <riazonbin@mail.ru>
2022-10-10 20:59:50 +0300

    added autogeneration button

--- CharacterEditorWPF/MatchPage.xaml.cs
+++ CharacterEditorWPF/MatchPage.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using CharacterEditorCore;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,6 +23,10 @@ namespace CharacterEditorWPF
     /// </summary>
     public partial class MatchPage : Page
     {
+        public int firstTeamCount = 0;
+        public int secondTeamCount = 0;
+        public const int maxTeamSize = 6;
+
         public MatchPage()
         {
             InitializeComponent();
@@ -28,6 +35,52 @@ namespace CharacterEditorWPF
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             NavigationService.GoBack();
+        }
+
+        private void btn_autoGeneration_Click(object sender, RoutedEventArgs e)
+        {
+            if(firstTeamCount == maxTeamSize && secondTeamCount == maxTeamSize)
+            {
+                firstTeamCount = 0;
+                secondTeamCount = 0;
+                lb_firstTeam.Items.Clear();
+                lb_secondTeam.Items.Clear();
+            }
+
+
+            Random random = new Random();
+            var collection = (MongoDBLink.MongoDB.GetCollection()).Find(new BsonDocument()).ToList();
+
+            while (firstTeamCount != maxTeamSize)
+            {
+                firstTeamCount++;
+
+
+                var randomCharacter = GetRandomCharacterFromList(collection);
+
+                lb_firstTeam.Items.Add(new CharacterInfo(randomCharacter._id, randomCharacter.Name));
+
+                collection.Remove(randomCharacter);
+            }
+
+            while (secondTeamCount != maxTeamSize)
+            {
+
+                var randomCharacter = GetRandomCharacterFromList(collection);
+
+                secondTeamCount++;
+
+                lb_secondTeam.Items.Add(new CharacterInfo(randomCharacter._id, randomCharacter.Name));
+
+                collection.Remove(randomCharacter);
+            }
+        }
+
+        private Character GetRandomCharacterFromList(List<Character> collection)
+        {
+            Random random = new Random();
+
+            return collection[random.Next(0, collection.Count)];
         }
     }
 }
